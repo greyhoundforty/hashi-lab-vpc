326b2ae5229c5e8fe5bbc260129d3674977b2d12
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-09-22 07:36:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,18 +56,18 @@ module "consul_cluster" {
   subnet_id         = module.subnet[count.index].id
   ssh_keys          = [data.ibm_is_ssh_key.regional_key.id]
   resource_group_id = data.ibm_resource_group.project.id
-  name              = "${var.project_name}-${data.ibm_is_zones.regional_zones.zones[count.index]}"
-  zone              = data.ibm_is_zones.regional_zones.zones[0]
-  security_groups   = [module.bastion.bastion_maintenance_group_id, module.security.consul_security_group]
+  name              = "${var.project_name}-consul-${count.index}"
+  zone              = data.ibm_is_zones.regional_zones.zones[count.index]
+  security_groups   = module.security.consul_security_group
   tags              = concat(var.tags, ["zone:${data.ibm_is_zones.regional_zones.zones[count.index]}", "region:${var.region}", "project:${var.project_name}"])
   user_data         = file("install.yml")
   allow_ip_spoofing = false
 }
 
-# resource "ibm_is_security_group_network_interface_attachment" "under_maintenance" {
-#   depends_on        = [module.consul_cluster]
-#   count             = 3
-#   network_interface = module.consul_cluster[count.index].instance.primary_network_interface.0.id
-#   security_group    = module.bastion.bastion_maintenance_group_id
-# }
+resource "ibm_is_security_group_network_interface_attachment" "under_maintenance" {
+  depends_on        = [module.consul_cluster]
+  count             = 3
+  network_interface = module.consul_cluster[count.index].instance.primary_network_interface.0.id
+  security_group    = module.bastion.bastion_maintenance_group_id
+}
 
